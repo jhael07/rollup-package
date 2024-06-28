b2aa413cebe9d5327a1bfcfcca088f01e7131b46
jhael07 <jhaelr007@gmail.com>
2024-06-27 19:36:18 -0700

    package

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./index.css";
+
+const App = () => {
+  return (
+    <button
+      style={{
+        backgroundColor: "#2a7dea",
+        borderRadius: "3.75rem",
+        borderWidth: "1px",
+        borderStyle: "solid",
+        borderColor: "black",
+        cursor: "pointer",
+        padding: "0.4rem",
+        paddingRight: "1rem",
+        paddingLeft: "1rem",
+      }}
+    >
+      Click here
+    </button>
+  );
+};
+
+export default App;
